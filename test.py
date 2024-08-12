47074754f91ea46e6058787733ac5986b8641317
Ramiro <rg@tokn1.com>
2024-08-11 22:45:39 -0300

    Adding more changes to start posting commets in the forums

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import math
 import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from chromedriver_autoinstaller import install as chrome_driver_install
@@ -9,6 +10,22 @@ from chromedriver_autoinstaller import install as chrome_driver_install
 
 BINANCE_URL = "https://p2p.binance.com/en/trade/sell/USDT?fiat=ARS&payment=all-payments"
 DOLARHOY_URL = "https://dolarhoy.com/"
+FOROBETA_URL = "https://forobeta.com/"
+MYPAYO_URL = "https://mipayo.com/foro/viewforum.php?f=14"
+
+# no pushear, y hacer archivo de configs
+FOROBETA_USERNAME = "RamonAbila.3"
+FOROBETA_PASSWORD = "-"
+MYPAYO_USERNAME = "ramonabila.3"
+MYPAYO_PASSWORD = "-"
+
+FOROBETA_POST_URL = ""
+MYPAYO_POST_URL = ""
+EXCHANGE_FORUM_MESSAGE = """Seguimos cambiando! Los espero por este medio o VIA Whatsapp!
+La mejor cotizacion SIEMPRE!
+
+https://wa.link/ramonabila"""
+
 SHORT_TIMEOUT = 1.5
 DRIVER_PATH = '../chromedriver.exe'
 RDA_COMISSION = 0.85
@@ -17,12 +34,15 @@ ONLY_PAYO = False
 
 def main_function():
     
+    # poner todos los waiters en base a una clase wait para que funcione todo mejor mas que nada forobeta que tardan en cargar
+    
     driver = driver_initialization()
     driver.maximize_window()
     driver.get(DOLARHOY_URL)
     
     RDA_COMISSION = float(input("Enter the commission to use in this execution (0.85 is the minimum): "))
     ONLY_PAYO = bool(input("Only Payoneer text? (YES/NO): "))
+    PUBLISH_COTIZATIONS = bool(input("Do you want to publish in forums? (YES/NO): "))
     
     buy_element = driver.find_element(By.CLASS_NAME, "compra").text
     sell_element = driver.find_element(By.CLASS_NAME, "venta").text
@@ -90,6 +110,28 @@ def main_function():
         print(str((cotization1+2)) + " mas de 800 USD Payoneer")
     
         
+    if PUBLISH_COTIZATIONS:
+        driver.get(FOROBETA_URL)
+        driver.find_element(By.XPATH, "//span[@class='p-navgroup-linkText' and text()='Acceder']").click()
+        time.sleep(1.5)
+        driver.find_element(By.XPATH, "//input[@name='login']").send_keys(FOROBETA_USERNAME)
+        driver.find_element(By.XPATH, "//input[@name='password']").send_keys(FOROBETA_PASSWORD)
+        driver.find_element(By.XPATH, "//button[contains(@class, 'button--primary') and contains(@class, 'button') and contains(@class, 'button--icon') and contains(@class, 'button--icon--login')]").click()
+        time.sleep(5)
+        
+        driver.get(FOROBETA_POST_URL)
+        driver.find_element(By.XPATH, "//div[@class='fr-wrapper show-placeholder']").send_keys(EXCHANGE_FORUM_MESSAGE)
+        driver.find_element(By.XPATH, "//span[contains(@class, 'button-text') and contains(text(), 'Responder')]").click()
+
+        try:
+            element = driver.find_element(By.XPATH, "//time[contains(text(), 'Hace un momento')]")
+            print("Comment posting successfully (Forobeta).")
+        except NoSuchElementException:
+            print("Comment posting generated an error (Forobeta).")
+
+        driver.get(MYPAYO_URL)
+        # cONTINUE
+    
     driver.quit()   
             
     
@@ -99,7 +141,7 @@ def driver_initialization():
         coptions = webdriver.ChromeOptions()
         coptions.add_argument('--ignore-certificate-errors')
         coptions.add_argument('--disable-extensions')
-        coptions.add_argument('--headless')
+        # coptions.add_argument('--headless')
         coptions.add_argument('--disable-gpu')
         coptions.add_argument('--no-sandbox')
         coptions.add_argument('--disable-dev-shm-usage')
